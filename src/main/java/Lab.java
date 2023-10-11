65a237c99de1f84ab87346afe2ed340c11e636d9
shamrin20 <shamrinmillet20@gmail.com>
2023-10-11 13:33:31 +0530

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,13 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            str.append(nums[i]);
+            if (i < nums.length - 1) {
+                str.append(" ");
+            }
+        }
+        return str.toString();
     }
 }
